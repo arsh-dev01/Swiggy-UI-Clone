76a596316ec5f441597899344bef51a6fa5a1edf
arsh-dev01 <165241406+arsh-dev01@users.noreply.github.com>
2024-07-27 15:52:06 +0530

    first commit

---
+++ Header&Footer/footer.js
@@ -0,0 +1,57 @@
+let FooterHTML = `
+      <div class="box1">
+        <img src="../logo.png" alt="" width="80px" />
+        <h2>Swiggy</h2>
+        <h5>@2024 Technology Pvt.Ltd</h5>
+      </div>
+      <div class="box2">
+        <h3>Company</h3>
+        <ul>
+          <li>About</li>
+          <li>Team</li>
+          <li>Swiggy</li>
+          <li>Swiggy</li>
+          <li>Swiggy</li>
+          <li>Swiggy</li>
+        </ul>
+      </div>
+      <div class="box3">
+        <div class="section">
+          <h3>Contact US</h3>
+          <ul>
+            <li>Help & Support</li>
+            <li>Patner with us</li>
+            <li></li>
+          </ul>
+        </div>
+        <div class="section">
+          <h3>Legal</h3>
+          <ul>
+            <li>Terms& Conditions</li>
+            <li>Cookies Policy</li>
+            <li>Praivcy Policy</li>
+            <li>Indjkjkajfd</li>
+          </ul>
+        </div>
+      </div>
+      <div class="box4">
+        <h3>We Delivery</h3>
+        <ul>
+          <li>Korean</li>
+          <li>Punjabi</li>
+          <li>Indian</li>
+          <li>Andhra</li>
+          <li>Kerala</li>
+          <li>Bengali</li>
+          <li>
+            <select name="select" id="select">
+              <option value="Other">Other</option>
+              <option value="Other">Other</option>
+              <option value="Other">Other</option>
+            </select>
+          </li>
+        </ul>
+      </div>
+        <link rel="stylesheet" href="../Header&Footer/footer.css" />
+`;
+document.querySelector("footer").innerHTML = FooterHTML;
